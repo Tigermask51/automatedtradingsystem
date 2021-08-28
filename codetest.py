9ac1202d32a795690153e5dcbf0ad97f10316ac3
yeongbokimm <yeongbo.kimm@gmail.com>
2021-08-28 14:51:39 +0900

    2day

--- codetest.py
+++ codetest.py
@@ -1,6 +1,12 @@
-f = open("upkey.txt")
-content_f = f.readlines()
-access = content_f[0].strip()
-secret = content_f[1].strip()
+import pyupbit
 
-print(access)
+aa = pyupbit.get_ohlcv("KRW-BTC", "day")
+print(aa)
+
+def cal_target(ticker):
+    df = pyupbit.get_ohlcv(ticker, "day")
+    yesterday = df.iloc[-2]
+    today = df.iloc[-1]
+    yesterday_range = yesterday['high']-yesterday['low']
+    target = today['open'] + yesterday_range * 0.5
+    return(target)
